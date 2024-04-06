24f8dcb40dbcff6dceb39812bff7d85847038ca8
Timothy Karani <kartimothy@gmail.com>
2024-04-06 13:42:06 +0300

    Can authenticate via asymmetric key

---
+++ dan-vega-jwt-symmetric-key/src/main/java/tech/c3n7/danvega/jwtsymmetrickey/controller/AuthController.java
@@ -0,0 +1,22 @@
+package tech.c3n7.danvega.jwtsymmetrickey.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.c3n7.danvega.jwtsymmetrickey.service.TokenService;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final TokenService tokenService;
+
+    public AuthController(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping("/token")
+    public String token(Authentication authentication) {
+        return tokenService.generateToken(authentication);
+    }
+}
---
+++ dan-vega-jwt-symmetric-key/src/main/java/tech/c3n7/danvega/jwtsymmetrickey/controller/HomeController.java
@@ -0,0 +1,13 @@
+package tech.c3n7.danvega.jwtsymmetrickey.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home(Authentication authentication) {
+        return "Hello " + authentication.getName();
+    }
+}
